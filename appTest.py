c17f02c7f67bf266e5ad0ed90688af8aafc080b2
Jasmine Yuen <jyuen30@stuy.edu>
2022-11-09 09:28:41 -0500

    register

--- appTest.py
+++ appTest.py
@@ -20,53 +20,26 @@ DB_FILE="database.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
-#Creates table and executes
-#db.execute("DROP TABLE if exists users")
 createUser = "CREATE TABLE if not exists users (username STRING, password STRING);"
 c.execute(createUser)
-
-#print("----COURSES TABLE----")
-
-#Fetch all rows from ONE data table
-# c.execute('SELECT * from courses;') #query
-# for i in c.fetchall(): #
-#     print(i) #Prints out each data 
-    
-# print("----STUDENTS TABLE----")
-
-# c.execute('SELECT * from students;') #query
-# for i in c.fetchall():
-#     print(i) #Prints out each data 
-
-# for i in range(len(students)):
-#     c.execute(f"INSERT INTO students VALUES (students['name'][{i}], students['age'][{i}], students['id'][{i}]);")
-
-# for i in range(len(courses)):
-#     c.execute(f"INSERT INTO students VALUES (courses['code'][{i}], courses['mark'][{i}], courses['id'][{i}]);")
-
-#==========================================================
-
-
 
 # FLASK
 
 app = Flask(__name__)
 
-#hard-coded login info
-username = "ziying"
-password = "john"
+# #hard-coded login info
+# username = "ziying"
+# password = "john"
 
 app.secret_key = "23bd2dcea35c795e204d397157f3d55bf1afda7db6519a46f9d1e5a5f02ed45b"
 
 def authenticate(user, passw):
     return (user == username and passw == password)
 
-def addUser(user, passw):
-    addUser = f"INSERT INTO users VALUES('{user}', '{passw}');"
-    c.execute(addUser)
-
-    db.commit() #save changes
-    db.close()  #close database
+# def addUser(user, passw):
+#     addUser = f"INSERT INTO users VALUES('{user}', '{passw}');"
+#     c.execute(addUser)
+#     db.commit() #save changes
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
@@ -76,28 +49,37 @@ def disp_loginpage():
     return render_template('login.html')
 
 
-@app.route(, methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     #authenticate login info from form using "request.args"
-    if authenticate(request.args['username'], request.args['password']):
-        #establishes a session
-        session['username'] = request.args['username']
-        #bring to new page
-        return render_template('response.html', functional="WORKS!", username=session['username'])
-    #error messages
-    else:
-        if username != request.args['username']:
-            return render_template('login.html', loginMSG="Bad username.")
-        return render_template('login.html', loginMSG="Bad password.")
+    if request.method=="POST": 
+        if authenticate(request.form['username'], request.form['password']):
+            #establishes a session
+            session['username'] = request.form['username']
+            #session['username'] = request.args['username']
+            #bring to new page
+            return render_template('response.html', functional="WORKS!", username=session['username'])
+            #error messages
+        else:
+            if username != request.form['username']:
+                return render_template('login.html', loginMSG="Bad username.")
+            return render_template('login.html', loginMSG="Bad password.")
 
+@app.route("/register", methods=['GET', 'POST'])
+def addUser(user, passw):
+    addUser = f"INSERT INTO users VALUES('{user}', '{passw}');"
+    c.execute(addUser)
+    db.commit() #save changes
 
-@app.route('/logout')
+@app.route('/logout', methods=['POST'])
 def logout():
     # remove the username from the session if it's there
-    session.pop('username', None)
-    return render_template('login.html', loginMSG="Logged out")
+    if request.method == "POST":
+        session.pop('username', None)
+        return render_template('login.html', loginMSG="Logged out")
 
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
+    db.close()  #close database
